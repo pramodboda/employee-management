7997099df946e3416177cec85241c6f89117e12b
Pramod Boda <pramodboda@Pramods-iMac.local>
2024-12-19 00:53:13 +0530

    UPDATE:Enhancing UI

---
+++ frontend-angular19/src/app/shared/components/navbar/navbar.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+import { MatToolbarModule } from '@angular/material/toolbar';
+@Component({
+  selector: 'app-navbar',
+  imports: [MatToolbarModule],
+  templateUrl: './navbar.component.html',
+  styleUrl: './navbar.component.scss',
+})
+export class NavbarComponent {}
