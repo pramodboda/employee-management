b750366dff40d0afaa79d106ee7b226d5edfe2ed
Pramod Boda <pramodboda@Pramods-iMac.local>
2025-01-10 03:27:42 +0530

    Organizing project folders

--- frontend-angular19/src/environments/environment.development.ts
+++ frontend-angular19/src/environments/environment.development.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:8080/api/v1/employees', // Example API URL for development
+  // apiUrl: 'http://localhost:8080/api/v1/employees', // Example API URL for development
+  apiUrl: 'https://emp-crud-sb.onrender.com/api/v1/employees', // Example API URL for production
   // someKey: 'dev-key', // Example key for development
 };
--- frontend-angular19/src/environments/environment.ts
+++ frontend-angular19/src/environments/environment.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
   apiUrl: 'http://localhost:8080/api/v1/employees', // Example API URL for development
+  // apiUrl: 'https://emp-crud-sb.onrender.com/api/v1/employees', // Example API URL for production
   // someKey: 'prod-key', // Example key for production
 };
